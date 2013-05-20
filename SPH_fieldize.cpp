daca951540f5f053a0d7db9255c091f12e2233ce
Simeon Bird <spb@ias.edu>
2013-05-20 16:37:44 +0100

    Make SPH fieldize work in 3d

--- SPH_fieldize.cpp
+++ SPH_fieldize.cpp
@@ -1,45 +1,23 @@
 #include <cmath>
 #include <algorithm>
+#include <stdio.h>
 
 
-/*Compute the SPH weighting for this cell, using the trapezium rule.
+/*Compute the SPH weighting for this cell.
  * rr is the smoothing length, r0 is the distance of the cell from the center*/
 double compute_sph_cell_weight(double rr, double r0)
 {
     if(r0 > rr){
         return 0;
     }
-    double total=0;
-    double h2 = rr*rr;
-    //Do the z integration with the trapezium rule.
-    //Evaluate this at some fixed (well-chosen) abcissae
-    double zc=0;
-    if(rr/2 > r0)
-        zc=sqrt(h2/4-r0*r0);
-    double zm = sqrt(h2-r0*r0);
-    double zz[5]={zc,(3*zc+zm)/4.,(zc+zm)/2.,(zc+3*zm)/2,zm};
-    double kern[5];
-    double kfac= 8/M_PI/pow(rr,3);
-    for(int i=0; i< 5;i++){
-        kern[i] = 2*pow(1-sqrt(zz[i]*zz[i]+r0*r0)/rr,3)*kfac;
+    double x = r0/rr;
+    double geom = 8/M_PI/pow(rr,3);
+    if(x > 0.5){
+        return geom*2*pow(1-x,3);
     }
-    for(int i=0; i< 4;i++){
-        //Trapezium rule. Factor of 1/2 goes away because we double the integral
-        total +=(zz[i+1]-zz[i])*(kern[i+1]+kern[i]);
+    else{
+        return geom*(1-6*x*x+6*pow(x,3));
     }
-    if(rr/2 > r0){
-        double zz2[9]={0,zc/16.,zc/8.,zc/4.,3*zc/8,zc/2.,5/8.*zc,3*zc/4.,zc};
-        double kern2[9];
-        for(int i=0; i< 9;i++){
-            double R = sqrt(zz2[i]*zz2[i]+r0*r0);
-            kern2[i] = kfac*(1-6*(R/rr)*R/rr+6*pow(R/rr,3));
-        }
-        for(int i=0; i< 8;i++){
-            //Trapezium rule. Factor of 1/2 goes away because we double the integral
-            total +=(zz2[i+1]-zz2[i])*(kern2[i+1]+kern2[i]);
-        }
-    }
-    return total;
 }
 
 
@@ -47,9 +25,9 @@ double compute_sph_cell_weight(double rr, double r0)
 #ifndef NO_KAHAN
 /*Evaluate one iteration of Kahan Summation: sum is the current value of the field,
  *comp the compensation array, input the value to add this time.*/
-inline void KahanSum(double* sum, double* comp, const double input, const int xoff, const int yoff, const int nx)
+inline void KahanSum(double* sum, double* comp, const double input, const int xoff,const int yoff, const int zoff, const int nx)
 {
-  const int off = nx*xoff+yoff;
+  const int off = nx*nx*xoff+nx*yoff+zoff;
   const double yy = input - *(comp+off);
   const double temp = *(sum+off)+yy;     //Alas, sum is big, y small, so low-order digits of y are lost.
   *(comp+off) = (temp - *(sum+off)) -yy; //(t - sum) recovers the high-order part of y; subtracting y recovers -(low part of y)
@@ -58,9 +36,10 @@ inline void KahanSum(double* sum, double* comp, const double input, const int xo
 
 #else
 
-inline void KahanSum(double* sum, double* comp, const double input, const int xoff, const int yoff, const int nx)
+inline void KahanSum(double* sum, double* comp, const double input, const int xoff,const int yoff, const int zoff, const int nx)
 {
-  *(sum+nx*xoff+yoff)+=input;
+  const int off = nx*nx*xoff+nx*yoff+zoff;
+  *(sum+off)+=input;
 }
 #endif
 
@@ -71,27 +50,32 @@ int SPH_interpolate(double * field, double * comp, const int nx, float *pos, flo
 {
     for(int p=0;p<nval;p++){
         //Temp variables
-        float pp[2];
-        pp[0]= pos[3*p+1];
-        pp[1]= pos[3*p+2];
+        float pp[3];
+        //Max size of kernel
+        int upgx[3], lowgx[3];
         float rr= radii[p];
         float val= value[p];
         double weight = 1;
         if (weights != NULL){
             weight = weights[p];
         }
-        //Max size of kernel
-        int upgx = floor(pp[0]+rr);
-        int upgy = floor(pp[1]+rr);
-        int lowgx = floor(pp[0]-rr);
-        int lowgy = floor(pp[1]-rr);
+        for(int i=0; i<3; i++)
+        {
+            pp[i] = pos[3*p+i];
+            upgx[i] = floor(pp[i]+rr);
+            lowgx[i] = floor(pp[i]-rr);
+        }
         //Try to save some integrations if this particle is totally in this cell
-        if (lowgx==upgx && lowgy==upgy && lowgx >= 0 && lowgy >= 0){
-                KahanSum(field, comp, val/weight, lowgx,lowgy,nx);
+        bool inside = true;
+        for(int i=0; i<3; i++)
+            inside *= (lowgx[i]==upgx[i] && lowgx[i] >= 0);
+
+        if(inside){
+                KahanSum(field, comp, val/weight, lowgx[0], lowgx[1], lowgx[2], nx);
                 continue;
         }
         /*Array for storing cell weights*/
-        double sph_w[upgy-lowgy+1][upgx-lowgx+1];
+        double sph_w[upgx[2]-lowgx[2]+1][upgx[1]-lowgx[1]+1][upgx[0]-lowgx[0]+1];
 
         /*Total of cell weights*/
         double total=0;
@@ -104,80 +88,48 @@ int SPH_interpolate(double * field, double * comp, const int nx, float *pos, flo
         for(int i=0; i < nsub; i++)
             subs[i] = (i+1.)/(1.*nsub+1);
         #pragma omp parallel for reduction(+:total)
-        for(int gy=lowgy;gy<=upgy;gy++)
-            for(int gx=lowgx;gx<=upgx;gx++){
-                sph_w[gy-lowgy][gx-lowgx]=0;
-                for(int iy=0; iy< nsub; iy++)
-                for(int ix=0; ix< nsub; ix++){
-                    double xx = gx-pp[0]+subs[ix];
-                    double yy = gy-pp[1]+subs[iy];
-                    double r0 = sqrt(xx*xx+yy*yy);
-                    sph_w[gy-lowgy][gx-lowgx]+=compute_sph_cell_weight(rr,r0)/nsub/nsub;
+        for(int gz=lowgx[2];gz<=upgx[2];gz++)
+            for(int gy=lowgx[1];gy<=upgx[1];gy++)
+                for(int gx=lowgx[0];gx<=upgx[0];gx++){
+                    sph_w[gz-lowgx[2]][gy-lowgx[1]][gx-lowgx[0]]=0;
+                    for(int iz=0; iz< nsub; iz++)
+                    for(int iy=0; iy< nsub; iy++)
+                    for(int ix=0; ix< nsub; ix++){
+                        double xx = gx-pp[0]+subs[ix];
+                        double yy = gy-pp[1]+subs[iy];
+                        double zz = gz-pp[2]+subs[iz];
+                        double r0 = sqrt(xx*xx+yy*yy+zz*zz);
+                        sph_w[gz-lowgx[2]][gy-lowgx[1]][gx-lowgx[0]]+=compute_sph_cell_weight(rr,r0)/nsub/nsub;
+                    }
+                    total+=sph_w[gz-lowgx[2]][gy-lowgx[1]][gx-lowgx[0]];
                 }
-                total+=sph_w[gy-lowgy][gx-lowgx];
-            }
-//         if(total > 1.05)
-//           tothigh++;
-//         if(total< 0.5)
-//           totlow++;
         if(total == 0){
-//            fprintf(stderr,"Massless particle detected! rr=%g gy=%d gx=%d nsub = %d pp= %g %g \n",rr,upgy-lowgy,upgx-lowgx, nsub,-pp[0]+lowgx,-pp[1]+lowgy);
+           fprintf(stderr,"Massless particle detected! rr=%g gy=%d gx=%d nsub = %d pp= %g %g \n",rr,upgx[1]-lowgx[1],upgx[0]-lowgx[0], nsub,-pp[0]+lowgx[0],-pp[1]+lowgx[1]);
             return 1;
         }
-        /* Some cells will be only partially in the array: only partially add them.
-         * Then add the right fraction to the total array*/
-
-        #pragma omp parallel for
-        for(int gy=std::max(lowgy,0);gy<=std::min(upgy,nx-1);gy++)
-            for(int gx=std::max(lowgx,0);gx<=std::min(upgx,nx-1);gx++){
-                KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx,gy,nx);
-            }
         //Deal with cells that have wrapped around the edges of the grid
-        if (periodic){
-            //Wrapping y over
+        if(periodic){
             #pragma omp parallel for
-            for(int gy=nx-1;gy<=upgy;gy++){
-                //Wrapping only y over
-                for(int gx=std::max(lowgx,0);gx<=std::min(upgx,nx-1);gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx,gy-(nx-1),nx);
-                }
-                //y over, x over
-                for(int gx=nx-1;gx<=upgx;gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx-(nx-1),gy-(nx-1),nx);
-                }
-                //y over, x under
-                for(int gx=lowgx;gx<=0;gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx+(nx-1),gy-(nx-1),nx);
+            for(int gz=lowgx[2];gz<=upgx[2];gz++){
+                //This deals with cells that have wrapped around the edges of the grid
+                int gzm = (gz + (nx-1)) % (nx-1);
+                for(int gy=lowgx[1];gy<=upgx[1];gy++){
+                    int gym = (gy + (nx-1)) % (nx-1);
+                    for(int gx=lowgx[0];gx<=upgx[0];gx++){
+                        int gxm = (gx + (nx-1)) % (nx-1);
+                        KahanSum(field, comp, val*sph_w[gz-lowgx[2]][gy-lowgx[1]][gx-lowgx[0]]/total/weight,gxm,gym,gzm,nx);
+                    }
                 }
             }
-            //Wrapping y under
+        }
+        else {
+        /* Add cells to the array discarding parts that have wrapped around*/
             #pragma omp parallel for
-            for(int gy=lowgy;gy<=0;gy++){
-                //Only y under
-                for(int gx=std::max(lowgx,0);gx<=std::min(upgx,nx-1);gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx,gy+(nx-1),nx);
-                }
-                //y under, x over
-                for(int gx=nx-1;gx<=upgx;gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx-(nx-1),gy+(nx-1),nx);
-                }
-                //y under, x under
-                for(int gx=lowgx;gx<=0;gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx+(nx-1),gy+(nx-1),nx);
-                }
-            }
-            //Finally wrap only x
-            #pragma omp parallel for
-            for(int gy=std::max(lowgy,0);gy<=std::min(upgy,nx-1);gy++){
-                //x over
-                for(int gx=nx-1;gx<=upgx;gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx-(nx-1),gy,nx);
-                }
-                //x under
-                for(int gx=lowgx;gx<=0;gx++){
-                    KahanSum(field, comp, val*sph_w[gy-lowgy][gx-lowgx]/total/weight,gx+(nx-1),gy,nx);
-                }
-            }
+            for(int gz=std::max(lowgx[2],0);gz<=std::min(upgx[2],nx-1);gz++)
+                for(int gy=std::max(lowgx[1],0);gy<=std::min(upgx[1],nx-1);gy++)
+                    for(int gx=std::max(lowgx[0],0);gx<=std::min(upgx[0],nx-1);gx++){
+                        KahanSum(field, comp, val*sph_w[gz-lowgx[2]][gy-lowgx[1]][gx-lowgx[0]]/total/weight,gx,gy,gz,nx);
+                    }
         }
     }
     return 0;
